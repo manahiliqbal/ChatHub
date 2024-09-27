55405283a50c846a53276fd63e1ba2907f3da83a
manahil <120031852+manahiliqbal@users.noreply.github.com>
2024-09-27 22:01:27 +0500

    theme changes

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -18,7 +18,6 @@ const MessageContainer = () => {
 				<NoChatSelected />
 			) : (
 				<>
-					{/* Header */}
 					<div className='bg-slate-50 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
 						<span className='text-pink-900 font-bold'>{selectedConversation.fullName}</span>
